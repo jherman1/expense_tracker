751bf851943dbf3648be21f5abf1342b949972ba
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-08-16 11:21:00 -0600

    added icon & icon mapping for categories, as well as date formatter

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,14 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
-enum Category {
-  food, travel, leisure, work
-}
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.fastfood_sharp,
+  Category.travel: Icons.flight_takeoff_sharp,
+  Category.leisure: Icons.movie_creation_outlined,
+  Category.work: Icons.work_outline_sharp
+};
 
 class Expense {
-
-  Expense({required this.title, required this.amount, required this.date, required this.category}) : id = uuid.v4();
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
@@ -16,5 +29,7 @@ class Expense {
   final DateTime date;
   final Category category;
 
-  
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
