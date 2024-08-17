607a6dadaf9b85ee6d52c56b7dd33a4d669ec48c
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-08-17 13:15:11 -0600

    Added code for form data validation

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -37,6 +37,33 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final _enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = _enteredAmount == null || _enteredAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invlid input'),
+          content: const Text(
+              'Please make sure a valid title, amount, date, and category was entered'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            )
+          ],
+        ),
+      );
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -115,10 +142,7 @@ class _NewExpenseState extends State<NewExpense> {
                   },
                   child: const Text('Cancel')),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense'),
               ),
             ],
